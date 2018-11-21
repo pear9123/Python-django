741428e91fb9c78095951ac4d3e06ce2c7427d66
pear9123 <pear9123@gmail.com>
2018-11-21 17:36:03 +0900

    게시판 및 관리자웹생성

--- mysite/elections/admin.py
+++ mysite/elections/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 
+from .models import Candidate, Poll
 from .models import Candidate
 # Register your models here.
 
 
 admin.site.register(Candidate)
+admin.site.register(Poll)
--- mysite/elections/views.py
+++ mysite/elections/views.py
@@ -6,6 +6,11 @@ from .models import Candidate
 # Create your views here.
 def index(request):
     candidate = Candidate.objects.all()
+    context = {'candidate':candidate}
+    return render(request, 'elections/index.html',context)
+
+def areas(request, area):
+    return HttpResponse(area)
     str = ''
     for candidate in candidate:
         str += "<p>{} 기호{}번({})<br>".format(candidate.name,
