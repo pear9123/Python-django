dff77f5b18e29f8341551a3c893e070866e64328
pear9123 <pear9123@gmail.com>
2018-12-18 18:25:09 +0900

    MYSQL 연결환경설정

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -76,8 +76,14 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        #'ENGINE': 'django.db.backends.sqlite3',
+        #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+		'ENGINE' : 'django.db.backends.mysql',
+		'NAME' : 'python_db',
+		'USER' : 'root',
+		'PASSWORD' : None,
+		'HOST' : 'localhost',
+		'PORT' : '3307',
     }
 }
 
